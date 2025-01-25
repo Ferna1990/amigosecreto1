23cfbe84cb6a5f7b8d148185e95ec31c066a8e09
Ferna1990 <fcrulo901312@gmail.com>
2025-01-24 21:32:39 -0300

    cargado de lista de nombres y limpieza del campo listo aun falta terminar las funciones para mostrar la lista y para mostrar el amigo elegido.

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+numeroaleatorio= 0
+
+let amigos= []
+
+amigo=''
+
+listado = []
+
+amigoAmostrar= []
+
+function vaciar(){
+    let valorcaja = document.getElementById('amigo')
+    valorcaja.value = ''
+}
+
+function agregarAmigo(){
+    amigo=document.getElementById('amigo').value
+    if (amigo){
+        amigos.push(amigo)
+        vaciar() 
+    }else{
+        alert('Ingresa un nombre!')
+    }
+    
+}
+
+
+
+function mostrarlista(){
+    let elementohtml = document.getElementById('listaAmigos');
+    elementohtml.innerHTML= '';  
+    for (var x of amigos){
+        elementohtml.appendChild(x)
+    }  
+}
+
+mostrarlista
+
+function generarnumeroaleatorio(){
+    return Math.floor(Math.random() * 11);
+}
+
+function sortearAmigo(){
+    numeroaleatorio= generarnumeroaleatorio()
+    let sorteado= document.getElementById('resultado')
+    sorteado.innerHTML(amigos[numeroaleatorio])
+}
+
+
+
+
+
